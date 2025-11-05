d11c4e45ea6f636195bafe1e9e4bdddd85345528
Antonn <gugle23v@gmail.com>
2025-11-05 09:43:48 +0400

    commit1.5-Снижение связанности через Dependency Injection

--- OTP/OTP/GIBDDPostNotificationSystem.cs
+++ OTP/OTP/GIBDDPostNotificationSystem.cs
@@ -7,6 +7,19 @@
         Unknown = 0
     }
 
+    public interface INotificationSender
+    {
+        void Send(string message);
+    }
+
+    public class PostNotificationSender : INotificationSender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Сообщение для всех постов ГИБДД: {0}", message);
+        }
+    }
+
     public interface IViolationHandler
     {
         void ProcessViolation();
@@ -36,7 +49,12 @@
         }
     }
 
-    public class ViolationHandlerFactory
+    public interface IViolationHandlerFactory
+    {
+        IViolationHandler CreateHandler(TrafficViolationType violationType);
+    }
+
+    public class ViolationHandlerFactory : IViolationHandlerFactory
     {
         public IViolationHandler CreateHandler(TrafficViolationType violationType)
         {
@@ -52,40 +70,104 @@
         }
     }
 
-    public abstract class NotificationBase
+    public interface IMessageValidator
     {
-        protected static void SendNotification(string message)
+        bool ValidateRecipients(bool isUrgentSituation, List<string> recipientPosts);
+        string GetErrorMessage(bool isUrgentSituation, List<string> recipientPosts);
+    }
+
+    public class MessageValidator : IMessageValidator
+    {
+        public bool ValidateRecipients(bool isUrgentSituation, List<string> recipientPosts)
         {
-            Console.WriteLine("Сообщение для всех постов ГИБДД: {0}", message);
+            return (!isUrgentSituation && recipientPosts.Count > 0) ||
+                   (isUrgentSituation && recipientPosts.Count > 0);
         }
 
-        protected string CreateStandardMessage(int postId, string location, string department)
+        public string GetErrorMessage(bool isUrgentSituation, List<string> recipientPosts)
+        {
+            if (isUrgentSituation && recipientPosts.Count <= 0)
+            {
+                return "Требуются получатели для экстренного уведомления!";
+            }
+            return "Сообщение успешно отправлено.";
+        }
+    }
+
+    public interface IMessageFormatter
+    {
+        string FormatStandardMessage(int postId, string location, string department);
+        string FormatAlertMessage(int alertLevel, string region, string district);
+    }
+
+    public class MessageFormatter : IMessageFormatter
+    {
+        public string FormatStandardMessage(int postId, string location, string department)
         {
             return $"Сообщение от поста №{postId}, местоположение: {location}. Отдел: {department}";
         }
 
-        protected string CreateAlertMessage(int alertLevel, string region, string district)
+        public string FormatAlertMessage(int alertLevel, string region, string district)
         {
             return $"Спецуведомление уровня {alertLevel}: регион {region}, затронут район {district}";
         }
+    }
 
-        protected void SendMultipleNotifications(List<string> recipients, string messageTemplate)
+    public class NotificationDistributor
+    {
+        private readonly INotificationSender _notificationSender;
+
+        public NotificationDistributor(INotificationSender notificationSender)
+        {
+            _notificationSender = notificationSender;
+        }
+
+        public void SendToMultipleRecipients(List<string> recipients, string messageTemplate)
         {
             foreach (var recipient in recipients)
             {
                 string personalizedMessage = $"{messageTemplate} | Получатель: {recipient}";
-                SendNotification(personalizedMessage);
+                _notificationSender.Send(personalizedMessage);
             }
         }
     }
 
-    public class TrafficPoliceNotificationService : NotificationBase
+    public interface ITrafficPoliceNotificationService
     {
-        private readonly ViolationHandlerFactory _violationFactory;
+        string DistributeMessageToPosts(
+            int postIdentifier,
+            string postLocation,
+            bool isUrgentSituation,
+            DateTime eventTime,
+            List<string> recipientPosts,
+            string departmentName,
+            string incidentDetails = null);
 
-        public TrafficPoliceNotificationService()
+        void ProcessTrafficViolation(TrafficViolationType violationType);
+        void ProcessTrafficViolation(int violationTypeCode);
+        void BroadcastRegionalAlert(int alertSeverity, string regionName, string[] affectedDistricts);
+    }
+
+    public class TrafficPoliceNotificationService : ITrafficPoliceNotificationService
+    {
+        private readonly INotificationSender _notificationSender;
+        private readonly IViolationHandlerFactory _violationFactory;
+        private readonly IMessageValidator _messageValidator;
+        private readonly IMessageFormatter _messageFormatter;
+        private readonly NotificationDistributor _notificationDistributor;
+
+        // Конструктор с dependency injection
+        public TrafficPoliceNotificationService(
+            INotificationSender notificationSender = null,
+            IViolationHandlerFactory violationFactory = null,
+            IMessageValidator messageValidator = null,
+            IMessageFormatter messageFormatter = null)
         {
-            _violationFactory = new ViolationHandlerFactory();
+            _notificationSender = notificationSender ?? new PostNotificationSender();
+            _violationFactory = violationFactory ?? new ViolationHandlerFactory();
+            _messageValidator = messageValidator ?? new MessageValidator();
+            _messageFormatter = messageFormatter ?? new MessageFormatter();
+            _notificationDistributor = new NotificationDistributor(_notificationSender);
         }
 
         public string DistributeMessageToPosts(
@@ -97,30 +179,17 @@
             string departmentName,
             string incidentDetails = null)
         {
-            if (!ValidateRecipients(isUrgentSituation, recipientPosts))
+            if (!_messageValidator.ValidateRecipients(isUrgentSituation, recipientPosts))
             {
-                return GetDistributionErrorMessage(isUrgentSituation, recipientPosts);
+                return _messageValidator.GetErrorMessage(isUrgentSituation, recipientPosts);
             }
 
-            string notificationContent = CreateStandardMessage(postIdentifier, postLocation, departmentName);
-            SendMultipleNotifications(recipientPosts, notificationContent);
+            string notificationContent = _messageFormatter.FormatStandardMessage(
+                postIdentifier, postLocation, departmentName);
+
+            _notificationDistributor.SendToMultipleRecipients(recipientPosts, notificationContent);
 
             return "Сообщение успешно доставлено всем постам";
-        }
-
-        private bool ValidateRecipients(bool isUrgentSituation, List<string> recipientPosts)
-        {
-            return (!isUrgentSituation && recipientPosts.Count > 0) ||
-                   (isUrgentSituation && recipientPosts.Count > 0);
-        }
-
-        private string GetDistributionErrorMessage(bool isUrgentSituation, List<string> recipientPosts)
-        {
-            if (isUrgentSituation && recipientPosts.Count <= 0)
-            {
-                return "Требуются получатели для экстренного уведомления!";
-            }
-            return "Сообщение успешно отправлено.";
         }
 
         public void ProcessTrafficViolation(TrafficViolationType violationType)
@@ -129,7 +198,6 @@
             handler.ProcessViolation();
         }
 
-        // Перегрузка для обратной совместимости
         public void ProcessTrafficViolation(int violationTypeCode)
         {
             TrafficViolationType violationType = (TrafficViolationType)violationTypeCode;
@@ -140,8 +208,8 @@
         {
             foreach (var district in affectedDistricts)
             {
-                string alertContent = CreateAlertMessage(alertSeverity, regionName, district);
-                SendNotification(alertContent);
+                string alertContent = _messageFormatter.FormatAlertMessage(alertSeverity, regionName, district);
+                _notificationSender.Send(alertContent);
             }
         }
     }
--- OTP/OTP/Program.cs
+++ OTP/OTP/Program.cs
@@ -6,7 +6,21 @@ namespace OTP
     {
         static void Main()
         {
-            var notificationService = new TrafficPoliceNotificationService();
+            // Создаем зависимости
+            INotificationSender notificationSender = new PostNotificationSender();
+            IViolationHandlerFactory violationFactory = new ViolationHandlerFactory();
+            IMessageValidator validator = new MessageValidator();
+            IMessageFormatter formatter = new MessageFormatter();
+
+            // Внедряем зависимости через конструктор
+            var notificationService = new TrafficPoliceNotificationService(
+                notificationSender: notificationSender,
+                violationFactory: violationFactory,
+                messageValidator: validator,
+                messageFormatter: formatter);
+
+            // Альтернативно: можно использовать значения по умолчанию
+            // var notificationService = new TrafficPoliceNotificationService();
 
             notificationService.DistributeMessageToPosts(
                 1234,
@@ -16,16 +30,55 @@ namespace OTP
                 new List<string> { "Пост_1", "Пост_2" },
                 "Центральный отдел",
                 "Дополнительная информация о происшествии");
-
-            // Использование enum для типов нарушений
             notificationService.ProcessTrafficViolation(TrafficViolationType.Speeding);
             notificationService.ProcessTrafficViolation(TrafficViolationType.RedLightRunning);
-
-            // Обратная совместимость с int
             notificationService.ProcessTrafficViolation(1);
 
             notificationService.BroadcastRegionalAlert(3, "Санкт-Петербург", new[] {
                 "Адмиралтейский район", "Приморский район" });
+
+            // Демонстрация гибкости - можно легко подменить реализации
+            TestWithCustomDependencies();
+        }
+
+        static void TestWithCustomDependencies()
+        {
+            Console.WriteLine("\n--- Тест с кастомными зависимостями ---");
+
+            // Кастомная реализация для тестирования
+            var testNotificationSender = new TestNotificationSender();
+            var testViolationFactory = new TestViolationHandlerFactory();
+
+            var testService = new TrafficPoliceNotificationService(
+                notificationSender: testNotificationSender,
+                violationFactory: testViolationFactory);
+
+            testService.ProcessTrafficViolation(TrafficViolationType.Speeding);
+        }
+    }
+
+    // Тестовые реализации для демонстрации слабой связанности
+    public class TestNotificationSender : INotificationSender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"[ТЕСТ] Отправка сообщения: {message}");
+        }
+    }
+
+    public class TestViolationHandlerFactory : IViolationHandlerFactory
+    {
+        public IViolationHandler CreateHandler(TrafficViolationType violationType)
+        {
+            return new TestViolationHandler();
+        }
+    }
+
+    public class TestViolationHandler : IViolationHandler
+    {
+        public void ProcessViolation()
+        {
+            Console.WriteLine("[ТЕСТ] Обработка нарушения");
         }
     }
 }
